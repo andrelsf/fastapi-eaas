952bc323c06f393cebd3e3959d3e6bba2076b5a8
Andre Ferreira <andre.dev.linux@gmail.com>
2021-12-22 21:17:21 -0300

    Modificada implementação do app main

--- project/app/main.py
+++ project/app/main.py
@@ -1,18 +1,32 @@
 # project/app/main.py
 
+import os, logging
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Depends
-
-from app.config import get_settings, Settings
-
-
-app = FastAPI()
+from app.api.resources import ping, encrypt, decrypt
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+log = logging.getLogger('uvicorn')
+
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    application.include_router(ping.router)
+    application.include_router(encrypt.router)
+    application.include_router(decrypt.router)
+
+    return application
+
+
+app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")

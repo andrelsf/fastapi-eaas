12c418630f4f96e8b5a5caa2a921993733e50b2d
Andre Ferreira <andre.dev.linux@gmail.com>
2021-12-22 21:20:30 -0300

    Resource para check resource ping

---
+++ project/app/api/resources/ping.py
@@ -0,0 +1,17 @@
+# project/app/api/resources/ping.py
+
+from fastapi import APIRouter, Depends
+
+from app.config import get_settings, Settings
+
+
+router = APIRouter()
+
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }

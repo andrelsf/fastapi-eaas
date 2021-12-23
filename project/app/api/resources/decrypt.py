6190d29fcbc4831afbeb2c5340dda7abe3bb9b59
Andre Ferreira <andre.dev.linux@gmail.com>
2021-12-22 21:18:57 -0300

    Resource de Decrypt data

---
+++ project/app/api/resources/decrypt.py
@@ -0,0 +1,31 @@
+# project/app/api/resources/decrypt.py
+
+import json
+from base64 import b64decode
+
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
+from fastapi import APIRouter, Depends
+
+from app.config import get_settings, Settings
+from app.api.http.requests import PostDecryptRequest
+
+
+router = APIRouter()
+
+
+@router.post("/crypto/decrypt")
+def postDecrypt(postDecryptRequest: PostDecryptRequest, settings: Settings = Depends(get_settings)):
+    try:
+        key = settings.aes_key
+        
+        cipheredContent = b64decode(postDecryptRequest.cipheredContent)
+        iv = b64decode(postDecryptRequest.iv)
+
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decryptedData = unpad(cipher.decrypt(cipheredContent), AES.block_size)
+
+        return json.loads(decryptedData)
+    except (ValueError, KeyError):
+        return {'error': "Incorrect decryption"}
